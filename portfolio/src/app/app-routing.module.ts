e45137ecda0f4ca3984be1c31fe79e92fe5d15ee
Magyar Dorina <magyardorina484@gmail.com>
2022-02-08 17:17:09 +0100

    add profile

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,11 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MessageComponent } from './pages/message/message.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ReferencesComponent } from './pages/references/references.component';
+import { SchoolComponent } from './pages/school/school.component';
+import { WorkComponent } from './pages/work/work.component';
 
 const routes: Routes = [
   {
     path: '',
     component: NavbarComponent,
+    children: [
+      {
+        path: 'profile',
+        component: ProfileComponent
+      },
+      {
+        path: 'school',
+        component: SchoolComponent
+      },
+      {
+        path: 'work',
+        component: WorkComponent
+      },
+      {
+        path: 'references',
+        component: ReferencesComponent
+      },
+      {
+        path: 'message',
+        component: MessageComponent
+      }
+    ]
   }
 ];
 
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -7,11 +7,21 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SchoolComponent } from './pages/school/school.component';
+import { WorkComponent } from './pages/work/work.component';
+import { ReferencesComponent } from './pages/references/references.component';
+import { MessageComponent } from './pages/message/message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    ProfileComponent,
+    SchoolComponent,
+    WorkComponent,
+    ReferencesComponent,
+    MessageComponent,
   ],
   imports: [
     BrowserModule,
