f031a9bca71236f6675ac6b311f52fa3b8619b4c
Magyar Dorina <magyardorina484@gmail.com>
2022-02-16 15:30:50 +0100

    translate update

--- portfolio/src/app/shared/_components/helper/languages/en.ts
+++ portfolio/src/app/shared/_components/helper/languages/en.ts
@@ -1,4 +1,68 @@
 export const locale = {
   lang: 'en',
-  data: {}
+  data: {
+    GENERIC: {
+      NAME: 'Dorina Magyar',
+      ROLE: 'Web Developer',
+      ACTION: {
+        SEND: 'Send',
+        VIEW_SOURCE_CODE: 'View source code',
+        VIEW_WEBSITE: 'View website'
+      }
+    },
+    PAGE: {
+      MESSAGES: {
+        CONTACT: 'Contact',
+        FIRST_NAME: 'First name',
+        LAST_NAME: 'Lats name',
+        EMAIL_ADDRESS: 'Email address',
+        MESSAGE: 'Message',
+        FULL_NAME: 'Full name',
+      },
+      PROFILE: {
+        ABOUT_MYSELF: 'About myself',
+        AGE: '23 years old',
+        RESIDENCE: 'Budapest',
+        TITLE: 'Self-employed',
+        ABOUT_MYSELF_DESCRIPTION: 'Mindig is színes egyéniségnek tartottam magam, szeretek új dolgokat kipróbálni és kalandvágyónak tartom magam. Szeretem a kihívásokat és az akadályok leküzdését. Ha kitűzők egy célt magam elé, semmi nem tántoríthat el addig, amíg el nem érem.',
+        COMMUNITY_ROLES: 'Community roles',
+        ELECTION_COMMITTEE_MEMBER: 'Election committee member',
+        ELECTION_DESCRIPTION: 'Éves választások lebonyolításában való részvétel',
+        PRESIDENT_OF_THE_EVENT_COMMITTEE: 'President of the event committee',
+        EVENTS_ORGANISATION: 'Organisation of events',
+        OTHER_TASKS: 'Other tasks',
+        HOBBIES: 'Hobbies',
+        BOARD_GAMES: 'Board games',
+        COMPUTER_GAMES: 'Computer games',
+        SNOWBOARDING: 'Snowboarding',
+        WORKOUT: 'Workout',
+        PAINTING: 'Painting'
+      },
+      SCHOOL: {
+        CERTIFICATIONS: 'Certifications',
+        ELTE: 'Eötvös Loránd University',
+        COMPUTER_SIENCE: 'Computer sience',
+
+        SZECHENYI: 'Széchényi István Secondary School of Economics, Informatics and College of Nyíregyháza',
+        GRADUATION: 'Graduate, Computer system maintenance',
+
+        SKILLS: 'Skills',
+        PROGRAMING_SKILLS: 'Programing skills',
+        APPLIED_SOFTWARE: 'Applied software'
+      },
+      REFERENCES: {
+        REFERENCE: 'References',
+        PERSONAL_PORTFOLIO: 'Personal portfolio',
+        THESIS: 'Thesis',
+        FULLSTACK: 'Fullstack university project',
+        EVE_JULIA: 'Eve Julia'
+      },
+      WORK: {
+        EXPERIENCE: 'Work experience',
+        TITLE_PROPAIR: 'Frontend developer',
+        TITLE_AIMOTIVE: 'Video annotation',
+        TITLE_HIGH_SCHOOL: 'Intern'
+      }
+    }
+  }
 }
--- portfolio/src/app/shared/_components/helper/languages/hu.ts
+++ portfolio/src/app/shared/_components/helper/languages/hu.ts
@@ -1,4 +1,68 @@
 export const locale = {
   lang: 'hu',
-  data: {}
+  data: {
+    GENERIC: {
+      NAME: 'Magyar Dorina',
+      ROLE: 'Webfejlesztő',
+      ACTION: {
+        SEND: 'Küldés',
+        VIEW_SOURCE_CODE: 'Forráskód megtekintése',
+        VIEW_WEBSITE: 'Weboldal megtekintése'
+      }
+    },
+    PAGE: {
+      MESSAGES: {
+        CONTACT: 'Kapcsolatfelvétel',
+        FIRST_NAME: 'Keresztnév',
+        LAST_NAME: 'Vezetéknév',
+        EMAIL_ADDRESS: 'Email-cím',
+        MESSAGE: 'Üzenet',
+        FULL_NAME: 'Teljes név',
+      },
+      PROFILE: {
+        ABOUT_MYSELF: 'Magamról',
+        AGE: '23 éves',
+        RESIDENCE: 'Budapest',
+        TITLE: 'Egyéni katás vállalkozó',
+        ABOUT_MYSELF_DESCRIPTION: 'Mindig is színes egyéniségnek tartottam magam, szeretek új dolgokat kipróbálni és kalandvágyónak tartom magam. Szeretem a kihívásokat és az akadályok leküzdését. Ha kitűzők egy célt magam elé, semmi nem tántoríthat el addig, amíg el nem érem.',
+        COMMUNITY_ROLES: 'Közösségi szerepkörök',
+        ELECTION_COMMITTEE_MEMBER: 'Választási bizottság tag',
+        ELECTION_DESCRIPTION: 'Éves választások lebonyolításában való részvétel',
+        PRESIDENT_OF_THE_EVENT_COMMITTEE: 'Szervező bizottság elnöke',
+        EVENTS_ORGANISATION: 'Rendezvények szervezése',
+        OTHER_TASKS: 'Egyéb feladatok',
+        HOBBIES: 'Hobbik',
+        BOARD_GAMES: 'Társasjátékok',
+        COMPUTER_GAMES: 'Számítógépes játékok',
+        SNOWBOARDING: 'Snowboard',
+        WORKOUT: 'Edzés',
+        PAINTING: 'Festés'
+      },
+      SCHOOL: {
+        CERTIFICATIONS: 'Képesítések',
+        ELTE: 'Eötvös Loránd Tudományegyetem',
+        COMPUTER_SIENCE: 'Programtervező informatikus',
+
+        SZECHENYI: 'Nyíregyházi Széchényi István Közgazdasági, Informatikai Szakközépiskola és Kollégium',
+        GRADUATION: 'Érettségi, Számítógépes rendszerkarbantartó',
+
+        SKILLS: 'Készségek',
+        PROGRAMING_SKILLS: 'Programozási készségek',
+        APPLIED_SOFTWARE: 'Alkalmazott szoftverek'
+      },
+      REFERENCES: {
+        REFERENCE: 'Referenciák',
+        PERSONAL_PORTFOLIO: 'Személyes portfolió',
+        THESIS: 'Szakdolgozat',
+        FULLSTACK: 'Fullstack egyetemi projekt',
+        EVE_JULIA: 'Eve Julia'
+      },
+      WORK: {
+        EXPERIENCE: 'Munkahelyi tapasztalatok',
+        TITLE_PROPAIR: 'Frontend fejlesztő',
+        TITLE_AIMOTIVE: 'Videó annotálás',
+        TITLE_HIGH_SCHOOL: 'Gyakornok'
+      }
+    }
+  }
 }
