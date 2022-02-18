4e83d0ddd3935c9d4443c3629a9744107461f2f1
Magyar Dorina <magyardorina484@gmail.com>
2022-02-18 15:19:18 +0100

    responsive site

--- portfolio/src/app/navbar/navbar.component.ts
+++ portfolio/src/app/navbar/navbar.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
-  constructor() { }
+export class NavbarComponent implements OnInit, OnDestroy {
+  mobileQuery!: MediaQueryList;
 
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
   ngOnInit(): void {
   }
+
 
 }
