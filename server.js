232f3241227b2dc9ea2dd0b0b7457dc56710aea3
Magyar Dorina <magyardorina484@gmail.com>
2022-02-21 10:39:44 +0100

    server js update

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/portfolio'));
+app.use(express.static('./dist/personalportfolio'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/portfolio/'}),
+    res.sendFile('index.html', {root: 'dist/personalportfolio/'}),
 );
 
 // Start the app by listening on the default Heroku port
